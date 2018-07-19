6df06143a972717e8153ec2eb1f3d189131efb30
foscomerlacci <foscomerlacci@tiscali.it>
2018-07-19 14:55:09 +0200

    corretto ultimo commit

--- static/cambia_beneficiario.js
+++ static/cambia_beneficiario.js
@@ -4,7 +4,7 @@
 
 (function($) {
 // inspect html to check id of category select dropdown.
-            $(document).on('change', "select#id_fk_beneficiario", function(){
+            $(document).on('focusin', "select#id_fk_beneficiario", function(){
 
 
                      $("select#id_asset").html('<option value="">---------</option>');
