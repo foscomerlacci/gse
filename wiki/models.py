e1e68e93f5d77526ddeddf7cf2707e57b7108540
foscomerlacci <foscomerlacci@tiscali.it>
2018-08-16 15:47:34 +0200

    rimossa controllo di unicità email su wiki/contatti

--- wiki/models.py
+++ wiki/models.py
@@ -8,7 +8,7 @@ class Contatto(models.Model):
     cognome = models.CharField(max_length=50 , blank=True)
     area = models.CharField(max_length=100, blank=True)
     telefono = models.CharField(max_length=20)
-    email = models.EmailField(unique=True, blank=True)
+    email = models.EmailField(blank=True)
     note = models.TextField(max_length=500, blank=True)
 
     def __str__(self):
